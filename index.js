f6531b851aa5b9f0f9d393e4a22ceaf13a823e7f
chengluyu <chengluyu@live.cn>
2017-12-15 11:12:23 +0800

    feat: add support for '=>' and '!='

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const path = require('path');
 const mkdirp = require('mkdirp');
 const os = require('os');
 const rimraf = require('rimraf');
+const leftPad = require('left-pad');
+const inspect = require('util').inspect
 
 const parser = new ArgumentParser({
   version: PackageJSON.version,
@@ -105,19 +107,68 @@ function addGlyphs(font, name, data) {
       }
     ]
   });
+  add('equal_greater.liga', {
+    "advanceWidth": 600,
+    "references": [
+      {
+        "glyph": "equal",
+        "x": -500,
+        "y": 0,
+        "a": 1.5,
+        "b": 0,
+        "c": 0,
+        "d": 1
+      },
+      {
+        "glyph": "greater",
+        "x": 0,
+        "y": 0,
+        "a": 1,
+        "b": 0,
+        "c": 0,
+        "d": 1
+      }
+    ]
+  });
+  add('not_equal.liga', {
+    "advanceWidth": 600,
+    "references": [
+      {
+        "glyph": "equal",
+        "x": -550,
+        "y": 0,
+        "a": 1.8,
+        "b": 0,
+        "c": 0,
+        "d": 1
+      },
+      {
+        "glyph": "slash",
+        "x": -300,
+        "y": 0,
+        "a": 1,
+        "b": 0,
+        "c": 0,
+        "d": 1
+      }
+    ]
+  });
 }
 
 function addRules(font) {
+
   const {lookups, features, languages} = font['GSUB'];
 
   function addLookup(name, value) {
+    // console.log('Lookup', name, 'Lookup Object', inspect(value, false, null));
     if (lookups[name]) {
-      throw new Error('lookup name already exists');
+      // throw new Error('lookup name already exists');
     }
     lookups[name] = value;
   }
 
   function addFeature(name, value) {
+    // console.log('Feature', name, value);
     if (features[name]) {
       throw new Error('feature name already exists');
     }
@@ -127,58 +178,106 @@ function addRules(font) {
     }
   }
 
-  addFeature("calt_00030", [
-    "lookup_calt_17"
-  ]);
+  function getMaxFeatureNumber() {
+    let num = 0;
+    for (const key of Object.keys(features)) {
+      const [, , id] = key.match(/^(\w{4})_(\d+)$/);
+      num = Math.max(num, parseInt(id, 10));
+    }
+    return num;
+  }
 
-  addFeature("ss06_00031", [
-    "lookup_ss06_18"
-  ]);
+  function getMaxLookupNumber() {
+    let num = 0;
+    for (const key of Object.keys(lookups)) {
+      const [, , id] = key.match(/^lookup_([\w\d]+)_(\d+)$/);
+      num = Math.max(num, parseInt(id, 10));
+    }
+    return num;
+  }
 
-  addLookup("lookup_calt_17", {
-    "type": "gsub_chaining",
-    "flags": {},
-    "subtables": [
-      {
-        "match": [
-          [ "hyphen" ],
-          [ "greater" ]
-        ],
-        "apply": [
-          {
-            "at": 0,
-            "lookup": "lookup_ss06_18"
-          }
-        ],
-        "inputBegins": 0,
-        "inputEnds": 1
-      },
-      {
-        "match": [
-          [ "LIG" ],
-          [ "greater" ]
-        ],
-        "apply": [
-          {
-            "at": 1,
-            "lookup": "lookup_ss06_18"
-          }
-        ],
-        "inputBegins": 1,
-        "inputEnds": 2
+  let featureCounter = getMaxFeatureNumber();
+  let lookupCounter = getMaxLookupNumber();
+
+  function addCaltFeature(lookups) {
+    const name = `calt_${leftPad(++featureCounter, 5, 0)}`;
+    addFeature(name, lookups);
+    return name;
+  }
+
+  function addSingleSub(dict) {
+    const name = `lookup_ss00_${++lookupCounter}`;
+    addLookup(name, {
+      type: 'gsub_single',
+      flags: {},
+      subtables: [dict]
+    });
+    return name;
+  }
+
+  function addChainingSub(glyphNames, applyLookup) {
+    const name = `lookup_calt_${++lookupCounter}`;
+    const subtables = [];
+    for (let i = 0; i < glyphNames.length; i++) {
+      const match = [];
+      for (let j = 0; j < i; j++) {
+        match.push(['LIG']);
       }
-    ]
-  });
-
-  addLookup("lookup_ss06_18", {
-    "type": "gsub_single",
-    "flags": {},
-    "subtables": [
-      {
-        "hyphen": "LIG",
-        "greater": "hyphen_greater.liga"
+      for (let j = i; j < glyphNames.length; j++) {
+        match.push([glyphNames[j]]);
       }
-    ]
+      subtables.push({
+        match,
+        apply: [{
+          at: i,
+          lookup: applyLookup
+        }],
+        inputBegins: i,
+        inputEnds: i + 1
+      });
+    }
+    addLookup(name, {
+      type: 'gsub_chaining',
+      flags: {},
+      subtables
+    });
+    return name;
+  }
+
+  function stringToGlyphNameSequence(str) {
+    return [...str].map(ch => {
+      const name = font.cmap[ch.charCodeAt(0)];
+      if (typeof name === 'string') {
+        return name;
+      }
+      throw new Error(`no glyph name for '${ch}'`);
+    })
+  }
+
+  function addProgrammingLigatures(dict) {
+    const chainings = [];
+    for (const from of Object.keys(dict)) {
+      const liga = dict[from];
+      const glyphNames = stringToGlyphNameSequence(from);
+      // generate single substitution map
+      const singleSub = {};
+      for (let i = 0; i < glyphNames.length - 1; i++) {
+        singleSub[glyphNames[i]] = 'LIG';
+      }
+      singleSub[glyphNames[glyphNames.length - 1]] = liga;
+      // add single substitution
+      const singleSubName = addSingleSub(singleSub);
+      // add chaining context sub
+      const chainingSubName = addChainingSub(glyphNames, singleSubName);
+      chainings.push(chainingSubName);
+    }
+    addCaltFeature(chainings);
+  }
+
+  addProgrammingLigatures({
+    '->': 'hyphen_greater.liga',
+    '=>': 'equal_greater.liga',
+    '!=': 'not_equal.liga'
   });
 }
 
